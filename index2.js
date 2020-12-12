d10632d6e032323e854dd223285da4fe276232f5
Mini Ware <codauknow@gmail.com>
2020-12-13 03:06:26 +0800

    added tips

--- index2.js
+++ index2.js
@@ -1,4 +1,6 @@
-var obj = "";
+messages = [""];
+//anger fear joy sadness analytical confident tentative
+attributes = [0,0,0,0,0,0,0];
 let uploadBtn = document.getElementById("upload");
 let documentTextField = document.getElementById("documentText");
 uploadBtn.onclick = () => {
@@ -28,10 +30,43 @@ uploadBtn.onclick = () => {
                 displayText += ": " ;
                 displayText += percentage;
                 displayText += "%\n"
+                if (toneName == "Anger"){
+                    attributes[0] = percentage;
+                }else if (toneName == "Fear"){
+                    attributes[1] = percentage;
+                }else if (toneName == "Joy"){
+                    attributes[2] = percentage;
+                }else if (toneName == "Sadness"){
+                    attributes[3] = percentage;
+                }else if (toneName == "Analytical"){
+                    attributes[4] = percentage;
+                }else if (toneName == "Confident"){
+                    attributes[5] = percentage;
+                }else if (toneName == "Tentative"){
+                    attributes[6] = percentage;
+                }
             }
             document.getElementById("data").innerHTML=displayText;
-
-
-            
+            //generate tips
+            displayText = "Tip:<br>";
+            if (attributes[0] >= 60){
+                displayText += "Calm down. ";
+            }else if (attributes[1] >= 60){
+                displayText += "Don't worry too much, everything will be fine. ";
+            }else if (attributes[2] <= 40){
+                displayText += "Cheer up! ";
+            }else if (attributes[3] >= 60){
+                displayText += "Brighten up your mood! ";
+            }else if (attributes[4] >= 60){
+                displayText += "Don't overthink too much. ";
+            }else if (attributes[5] <= 40){
+                displayText += "I believe in you! You can do it! ";
+            }else if (attributes[6] >= 60){
+                displayText += "Have more faith in yourself! ";
+            }
+            if (displayText == ""){
+                displayText = "You are doing great!";
+            }
+            document.getElementById("note").innerHTML=displayText;
         });
 }
